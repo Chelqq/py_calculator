68775bef7a629b0a40945fe9b1f75e9dedb78470
Chel <cdeszz8@gmail.com>
2023-03-13 00:11:13 -0600

    fixed a bug where all variables were str and couldnt operate them couldnt convert to int in the same line bc a weird bug.
    Almost done, need to return results and cleaning interface

--- calc1.py
+++ calc1.py
@@ -5,6 +5,7 @@ Created on Sun Mar 12 15:43:55 2023
 @author: XeL
 """
 
+from time import sleep
 n1 = n2 = op = 0
 
 print("----------Hello n welcome!----------\n\n")
@@ -16,16 +17,44 @@ print("3-Decimal division\n")
 print("4-Floor division\n")
 print("5-Multiplication\n")
 print("6-Exponentiation\n")
-print("7-Modulus\n\n\n")
+print("7-Modulo\n\n\n")
 
 op = input("Enter option: ")
 while not op.isdigit() or int(op) < 1 or int(op) > 8:
     op = input("Invalid Option. Enter option: ")
+op = int(op)
     
 n1 = input("\n\nThx!, pls enter number 1: ")
 while not n1.isdigit():
     n1 = input("Invalid Option. Pls enter number 1: ")
+n1 = int(n1)
 
 n2 = input("\n\nGreat!, pls enter number 2: ")
 while not n2.isdigit():
     n2 = input("Invalid. Pls enter number 2: ")
+n2=int(n2)
+
+sleep(1)
+print("\n\nthinking...\n")
+sleep(2)
+print("beep bop\n")
+sleep(2)
+print("ran out of fingers, but the anwser is: ")
+
+if op == '1':     #add
+    print(int(n1+n2))
+if op == 2:
+    print("")
+if op == 3:
+    print("")
+if op == 4:
+    print("")
+if op == 5: 
+    print("")
+if op == 6:
+    print("")
+if op == 7:
+    print("")
+    
+    
+
